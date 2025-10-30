6d7c4ba389faad2db99a7912362e58ab2c331e52
clozoya1729 <clozoya1729@gmail.com>
2025-10-30 13:54:54 -0600

    refactor canvas drawing

--- src/wasp.js
+++ src/wasp.js
@@ -389,37 +389,38 @@ function draw_line(canvas, start, end, color = '#888', linewidth = 1, alpha = 1)
     canvas.save();
     canvas.strokeStyle = color;
     canvas.lineWidth = linewidth;
+    canvas.globalAlpha = alpha;
     canvas.beginPath();
     canvas.moveTo(start[0], start[1]);
     canvas.lineTo(end[0], end[1]);
-    canvas.globalAlpha = alpha;
     canvas.stroke();
+}
+
+function draw_tangent(canvas, start, theta, color='#888', linewidth=linewidth, alpha=1) {
+    canvas.save();
+    canvas.strokeStyle = color;
+    canvas.lineWidth = linewidth;
+    canvas.globalAlpha = alpha;
+    canvas.beginPath();
+    canvas.moveTo(start.x, start.y);
+    canvas.lineTo(start.x + 44 * Math.cos(theta), start.y + 44 * Math.sin(theta));
+    canvas.setLineDash([6, 4]);
+    canvas.stroke();
+    canvas.setLineDash([]);
+    canvas.restore();
 }
 
 function draw_circle(canvas, radius, centroid, color = '#6d1b7b', linewidth = 1, alpha = 1, fill = false) {
     canvas.save();
+    canvas.globalAlpha = alpha;
+    canvas.lineWidth = linewidth;
+    canvas.strokeStyle = color;
     canvas.beginPath();
     canvas.arc(centroid[0], centroid[1], radius, 0, 2 * Math.PI);
     if (fill) {
         canvas.fill();
     }
-    canvas.globalAlpha = alpha;
-    canvas.lineWidth = linewidth;
-    canvas.strokeStyle = color;
     canvas.stroke();
-    canvas.restore();
-}
-
-function draw_tangent(canvas, p, theta, color) {
-    canvas.save();
-    canvas.beginPath();
-    canvas.moveTo(p.x, p.y);
-    canvas.lineTo(p.x + 44 * Math.cos(theta), p.y + 44 * Math.sin(theta));
-    canvas.strokeStyle = color;
-    canvas.lineWidth = 1;
-    canvas.setLineDash([6, 4]);
-    canvas.stroke();
-    canvas.setLineDash([]);
     canvas.restore();
 }
 
